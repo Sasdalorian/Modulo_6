f4cdac76ca0bef53ca8ba4e288378381af424fc1
Sasdalorian <randomweas1100@gmail.com>
2023-03-09 21:55:31 -0300

    abp1 Terminado

--- abp1/app.js
+++ abp1/app.js
@@ -1,79 +1,75 @@
-// console.log(parseInt(process.argv[2]) + parseInt(process.argv[3]) + parseInt(process.argv[4]));
-// let preguntas = ["Decime como ta llamass cabronazo \n", "Decime tu apeshido igual mamahuevo \n"];
-// let respuestas = [];
-
-// function preguntar(indice) {
-//     process.stdout.write(preguntas[indice])
-// }
-
-// process.stdin.on('data', (data) =>{
-//     respuestas.push(data.toString().trim());
-//     if(respuestas.length < preguntas.length){
-//         console.log(respuestas.length);
-//         preguntar(respuestas.length);
-//     } else {
-//         console.log(respuestas.toString())
-//         process.exit()
-//     }
-// });
-
-// preguntar(0);
-
-let preguntas = ["Fecha Inicio 'Año/Mes/Dia' \n", "Fecha Termino \n"];
+let preguntas = ["Fecha Inicio 'Año/Mes/Dia' \n", "Fecha Termino 'Año/Mes/Dia'\n"];
 let respuestas = [];
-let cantHoras = 0;
+
 
 function preguntar(p) {
     process.stdout.write(preguntas[p]);
 }
 
+let fechainicio = "";
+let fechatermino = "";
+let diasTrabajados = [];
+
+
 process.stdin.on('data', (data) => {
-    respuestas.push(data.toString().trim());
     const fecha = new Date(data.toString().trim());
-    contadorHoras(fecha.getDay());
-
-    console.log(fecha.getDay());
-    console.log(pagaDiaria);
-
-    function contadorHoras(dia) {
-        if(dia === 1 || dia === 3 || dia === 5) {
-            pagaDiaria = 7100 * 7;
-        } else if (dia === 2 || dia ===4) {
-            pagaDiaria = 7100 * 8;
-        } else if (dia === 6){
-            pagaDiaria = 12300 * 5;
-        } else return;
-    }
     
-})
-
-preguntar(0);
-
-/*
-let cantidadHorasLV = 16;
-const valorHoraLV = 7100;
-const subtotalLV = 113600;
-let cantidadHorasSA = 5;
-const valorHoraSA = 12300;
-const subtotalSA = 61500;
-const total = subtotalLV + subtotalSA;
-*/
-
-/*
+    //Para pasar a las siguiente pregunta o terminar
     respuestas.push(data.toString().trim());
     if (respuestas.length < preguntas.length) {
-        preguntar(respuestas.length);
-    } else {
-        console.log(`
-        Cantidad Horas LU-VI: ${cantidadHorasLV[0]}
-        Valor Hora: ${valorHoraLV}
-        Subtotal LU-VI: ${subtotalLV} \n
-        Cantidad Horas SA: ${cantidadHorasSA}
-        Valor Hora: ${valorHoraSA}
-        Subtotal SA: ${subtotalSA} \n
-        Total: ${subtotalLV} + ${subtotalSA} = ${total}
-        `);
-        process.exit();
+        preguntar(respuestas.length)
+    } else { 
+        if(respuestas[0]) {
+            fechainicio = new Date(respuestas[0]);
+        }
+        if(respuestas[1]) {
+            fechatermino = new Date(respuestas[1]); 
+        }
+
+    let fechaactual = fechainicio;
+    while (fechaactual <= fechatermino){
+        diasTrabajados.push(fechaactual.getDay(fechaactual));
+        fechaactual = new Date(fechaactual.setDate(fechaactual.getDate() +1));
     }
 
-*/
+    if (diasTrabajados.length > 0) {
+        contadorHoras(diasTrabajados);
+    }
+    process.exit();
+    }
+})
+//Calcula las horas trabajadas dependiendo del día
+let horasLV = 0;
+let horasSA = 0;
+let cumLV = 0;
+let cumSA = 0;
+let pagaDiaria = 0;
+
+function contadorHoras(diasTrabajados) {
+    diasTrabajados.forEach(dia => {
+        if(dia === 1 || dia === 3 || dia === 5) {
+            pagaDiaria = 7100 * 7;
+            cumLV = cumLV + pagaDiaria;
+            horasLV = horasLV + 7;
+        } else if (dia === 2 || dia ===4) {
+            pagaDiaria = 7100 * 8;
+            cumLV = cumLV + pagaDiaria;
+            horasLV = horasLV + 8;
+        } else if (dia === 6){
+            pagaDiaria = 12300 * 5;
+            cumSA = cumSA + pagaDiaria;
+            horasSA = horasSA + 5;
+        }
+    })
+    console.log(`
+    Cantidad Horas LU-VI: ${horasLV}
+    Valor Hora: $7100
+    Subtotal LU-VI: ${cumLV} \n
+    Cantidad Horas SA: ${horasSA}
+    Valor Hora: 12300
+    Subtotal SA: ${cumSA} \n
+    Total: ${cumLV} + ${cumSA} = ${cumLV + cumSA}
+    `)
+}
+
+preguntar(0);
