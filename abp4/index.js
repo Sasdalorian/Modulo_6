612d568906141602c58f52237899e0dc9af37e66
Sasdalorian <esteban.nicolas.sd@gmail.com>
2023-03-15 13:34:50 -0300

    abp4 avanzando

--- abp4/index.js
+++ abp4/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const hbs = require("hbs");
-const archivo = require("fs");
+const fs = require("fs");
 const path = "./json/almuerzos.json";
+const almuerzos = JSON.parse(fs.readFileSync(path, {encoding: "utf-8"}))
 
 app.set("view engine", "hbs");
 app.set("views", "./views");
@@ -10,4 +11,22 @@ app.use(express.static("public"));
 
 app.listen(3000);
 
-app.get("/", (req,res) => res.render("index", {resultado: JSON.parse(archivo.readFileSync(path, {encoding: "utf-8"}))}));
+app.get("/", (req,res) => res.render("index", {resultado: almuerzos}));
+
+app.get("/agregar/:nombre/:precio", (req, res) => {
+    const nombreU = req.params.nombre;
+    const precioU = req.params.precio;
+    let almuerzoA = {
+        nombre: nombreU,
+        precio: parseInt(precioU)
+    }
+
+    almuerzos.push(almuerzoA)
+    fs.writeFile(path, JSON.stringify(almuerzos), (err) => {})
+    res.send('Se ha subido el nuevo platillo');
+})
+
+app.get("/eliminar/:nombre", (req, res) => {
+    const nombreU = req.params.nombre;
+    
+})
